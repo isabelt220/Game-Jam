2f140c3fe28f0b18c87649158d614f1725eee10c
Isabel Tang <isabeltang220@gmail.com>
2018-09-15 14:16:05 -0400

    Added floor basics

---
+++ Assets/Scripts/FloorBehaviour.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloorBehaviour : MonoBehaviour {
+    public enum TileType {
+        Normal, Lava
+    }
+
+    [SerializeField] GameObject normalFloorTile;
+    [SerializeField] GameObject lavaFloorTile;
+
+    private TileType currentType;
+
+    public void Initialize(TileType initialType) {
+        this.currentType = initialType;
+
+        this.ReloadTileView();
+    }
+
+    public void SwitchFloorTiles () {
+        switch (this.currentType)
+        {
+            case TileType.Normal:
+                this.currentType = TileType.Lava;
+                break;
+            case TileType.Lava:
+                this.currentType = TileType.Normal;
+                break;
+        }
+
+        this.ReloadTileView();
+    }
+
+    private void ReloadTileView() {
+        this.normalFloorTile.SetActive(false);
+        this.lavaFloorTile.SetActive(false);
+
+        switch (this.currentType)
+        {
+            case TileType.Normal:
+                this.normalFloorTile.SetActive(true);
+                break;
+            case TileType.Lava:
+                this.lavaFloorTile.SetActive(true);
+                break;
+        }
+    }
+}
---
+++ Assets/Scripts/MazeBuilder.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+
+using UnityEngine;
+
+public class MazeBuilder : MonoBehaviour
+{
+    private static readonly FloorBehaviour.TileType[,] tiles = {
+        { FloorBehaviour.TileType.Normal, FloorBehaviour.TileType.Normal, FloorBehaviour.TileType.Normal },
+        { FloorBehaviour.TileType.Normal, FloorBehaviour.TileType.Lava, FloorBehaviour.TileType.Normal },
+        { FloorBehaviour.TileType.Normal, FloorBehaviour.TileType.Normal, FloorBehaviour.TileType.Normal }
+    };
+
+    [SerializeField] private FloorBehaviour floorPrefab;
+
+    private void Start()
+    {
+        this.BuildMaze();
+    }
+
+    private void BuildMaze() {
+        for (int i = 0; i < tiles.GetLength(0); i++) {
+            for (int j = 0; j < tiles.GetLength(1); j++) {
+                FloorBehaviour newTile = this.InstantiateFloorTiles(tiles[i,j]);
+                Vector3 newPosition = newTile.transform.position;
+
+                newPosition.x = i;
+                newPosition.z = j;
+
+                newTile.transform.position = newPosition;
+            }
+        }
+    }
+
+    private FloorBehaviour InstantiateFloorTiles(FloorBehaviour.TileType type)
+    {
+        FloorBehaviour newTile = GameObject.Instantiate<FloorBehaviour>(this.floorPrefab, this.transform);
+        newTile.Initialize(type);
+        return newTile;
+    }
+}
